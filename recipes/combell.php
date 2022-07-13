b3788ccacea9660c0004572cb41c6ee0b24a4cac
tombroucke <tom@tombroucke.be>
2022-07-13 11:41:57 +0200

    Update combell.php

--- recipes/combell.php
+++ recipes/combell.php
@@ -4,21 +4,48 @@ namespace Deployer;
 /** Reload PHP */
 desc('Reload PHP');
 task('combell:reloadPHP', function () {
-    run('reloadPHP.sh');
+    sleep(5);
+    reloadPhp();
 
     writeln('Waiting for opcache to reload');
-    $find = '20220712195907';
+    $revision = get('release_revision');
+    writeln('searching for revision ' . $revision);
+
+    $sleep = 10;
+    $iterations = 0;
     $start = microtime(true);
-    while (!revisionHasBeenUpdated($find)) {
-        sleep(1);
-        info('.');
+    while (!revisionHasBeenUpdated($revision)) {
+        sleep($sleep);
+        switch ($iterations) {
+            case 3:
+                writeln('Revision was not found after 30 seconds, reloading PHP');
+                reloadPhp();
+                break;
+            case 6:
+                writeln('Revision was not found after 60 seconds, reloading PHP');
+                reloadPhp();
+                break;
+            case 9:
+                writeln('Revision was not found after 90 seconds, reloading PHP');
+                reloadPhp();
+                break;
+        }
+        if($iterations == 12) {
+            writeln('Revision was not found after 120 seconds, continuing');
+            break;
+        }
+        $iterations++;
     }
     $timeElapsed = microtime(true) - $start;
     writeln(sprintf('OPcache reloaded after %ss.', $timeElapsed));
 
 });
 
+function reloadPhp() {
+    run('reloadPHP.sh');
+}
+
 function revisionHasBeenUpdated($revision) {
     $revisionFileContent = fetch(rtrim(get('url'), '/') . '/revision.txt');
-    return strpos($revisionFileContent, $revision) === 0;
+    return strpos($revisionFileContent, $revision) === 15;
 }
