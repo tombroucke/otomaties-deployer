e0a95c85eead9dca53e34247b680f0f5ba0d4266
Tom Broucke <tom@tombroucke.be>
2022-08-17 22:10:43 +0200

    fix(cleanup): Remove copy pasted line

--- recipes/wp-rocket.php
+++ recipes/wp-rocket.php
@@ -7,7 +7,6 @@ task('wp_rocket:init_cache', function () {
     within(
         '{{release_path}}',
         function () {
-            run('cd {{release_path}}/{{sage/theme_path}} && {{bin/composer}} install {{composer_options}}');
             run('wp rocket regenerate --file=advanced-cache');
             run('wp rocket clean --confirm');
             run('wp rocket preload');
