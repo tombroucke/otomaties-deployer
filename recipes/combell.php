942e05eb82fe7473ead8d3e7077f5e7a364c2cea
Tom Broucke <tom@tombroucke.be>
2022-08-17 21:44:57 +0200

    feat(combell): Reset OPcode cache

--- recipes/combell.php
+++ recipes/combell.php
@@ -7,7 +7,7 @@ task('combell:reloadPHP', function () {
     sleep(5);
     reloadPhp();
 
-    writeln('Waiting for opcache to reload');
+    writeln('Waiting for Combell to reload');
     $revision = get('release_revision');
     writeln('searching for revision ' . $revision);
 
@@ -30,22 +30,39 @@ task('combell:reloadPHP', function () {
                 reloadPhp();
                 break;
         }
-        if($iterations == 12) {
+        if ($iterations == 12) {
             writeln('Revision was not found after 120 seconds, continuing');
             break;
         }
         $iterations++;
     }
     $timeElapsed = microtime(true) - $start;
-    writeln(sprintf('OPcache reloaded after %ss.', $timeElapsed));
-
+    writeln(sprintf('Combell reloaded after %ss.', $timeElapsed));
 });
 
-function reloadPhp() {
+/** Reset OPcode cache */
+desc('Reset OPcode cache');
+task('combell:reset_opcode_cache', function () {
+    writeln('Writing opcache_reset file');
+    run('echo "<?php opcache_reset();" > {{release_path}}/www/opcache_reset.{{release_revision}}.php');
+    fetch(rtrim(get('url'), '/') . '/opcache_reset.' . get('release_revision') . '.php', info: $info);
+
+    if ($info['http_code'] === 200) {
+        writeln('OPcode cache has been reset');
+        writeln('Deleting opcache_reset file');
+        run('rm {{release_path}}/www/opcache_reset.{{release_revision}}.php');
+    } else {
+        writeln('Could not reset OPcode cache');
+    }
+});
+
+function reloadPhp()
+{
     run('reloadPHP.sh');
 }
 
-function revisionHasBeenUpdated($revision) {
+function revisionHasBeenUpdated($revision)
+{
     $revisionFileContent = fetch(rtrim(get('url'), '/') . '/revision.txt');
     return strpos($revisionFileContent, $revision) === 15;
 }
