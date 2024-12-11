36dfd52b6ec38284f81ffced63ac3c81f3185552
tombroucke <tom@tombroucke.be>
2024-12-11 13:42:54 +0100

    Update banned-urls.sql

--- snippets/wordfence/banned-urls.sql
+++ snippets/wordfence/banned-urls.sql
@@ -1,6 +1,6 @@
 INSERT INTO `{{ db_prefix }}wfconfig` (`name`, `val`, `autoload`)
 VALUES 
-	('bannedURLs', '/.env,/*.env,/wp-config.php*,/wp-config.txt,/*.old,/*.ini,/.git,/vendor*,/*.log,/wp-vcd.php,/xmlrpc.php,/admin*,/wanted/adm.php,/pvp*,/pvp.php,/admiinn,/adm.php,/*.temp,/*.bak,/adminer.php,/adminer*,/phpMyAdmin*,/pma,/*wp-config.php*,/*wlwmanifest.xml', 'yes')
+	('bannedURLs', '/*.sql,/.env,/*.env,/wp-config.php*,/wp-config.txt,/*.old,/*.ini,/.git,/vendor*,/*.log,/wp-vcd.php,/xmlrpc.php,/admin*,/wanted/adm.php,/pvp*,/pvp.php,/admiinn,/adm.php,/*.temp,/*.bak,/adminer.php,/adminer*,/phpMyAdmin*,/pma,/*wp-config.php*,/*wlwmanifest.xml,/installer.php,/.env.local,/.aws/*,/_cat/indices,/api/php.php,/.env.bak,/.env.php,/.env.backup,/.env.prod,/.env.test,/.env.dev,/.env.production,/.env.staging,/telescope/requests,/.vscode,/pma,/PMA,/swagger,/recentservers.xml,/sftp-config.json,/aws.json,/aws.yml,/config/default.json,/filezilla.xml,/FileZilla.xml,/sitemanager.xml,/app.ini,/db.zip,/production.ini,/config.ini,/database.tar.gz,/database.sql.gz,/.docker,/db.sql.gz,/Dockerfile*', 'yes')
 ON DUPLICATE KEY UPDATE 
 	`val` = VALUES(`val`), 
 	`autoload` = VALUES(`autoload`);
