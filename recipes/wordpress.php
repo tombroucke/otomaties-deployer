9a243c3e5ec92b2f81a94457a52f586057d134f9
Tom Broucke <tom@tombroucke.be>
2025-06-04 09:24:31 +0200

    ✨ feat: Added wp:cli command

--- recipes/wordpress.php
+++ recipes/wordpress.php
@@ -22,3 +22,13 @@ desc('Allow editors to edit theme options');
 task('wordpress:editor_edit_theme_options', function () {
     runWpQuery('wp cap add editor edit_theme_options');
 });
+
+desc('Run WP CLI command');
+task('wp:cli', function () {
+    $command = input()->getOption('cmd');
+    if (empty($command)) {
+        throw new \RuntimeException('You must provide a WP CLI command to run using the --cmd option.');
+    }
+
+    runWpQuery($command);
+})->once();
