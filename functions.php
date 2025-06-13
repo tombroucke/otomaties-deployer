6d44e9883439338e1edba20130d30dd928060678
Tom Broucke <tom@tombroucke.be>
2025-06-13 08:50:28 +0200

    🔨 refactor: Simplify code

--- functions.php
+++ functions.php
@@ -35,24 +35,26 @@ function replacePlaceholders($query)
     preg_match_all('/{{\s(.*?)(?::(.*?))?\s}}/', $query, $matches, PREG_SET_ORDER);
 
     $url = parse_url(get('url'), PHP_URL_HOST);
+
     $defaults = [
         'domain_no_extension' => preg_replace('/\.[^.]*$/', '', $url),
         'domain_extension' => $url,
     ];
 
-    foreach ($matches as $match) {
-        $replace = $match[0];
-        $key = $match[1];
-        $defaultValue = $match[2] ?? $defaults[$key] ?? '';
-        $value = get($key);
+    collect($matches)
+        ->each(function ($match) use (&$query, $defaults) {
+            $replace = $match[0];
+            $key = $match[1];
+            $defaultValue = $match[2] ?? $defaults[$key] ?? '';
+            $value = get($key);
 
-        if (! filled($value)) {
-            $defaultValue = filled($defaultValue) ? $defaultValue : null;
-            $value = ask("Enter a value for {$key}", $defaultValue);
-        }
+            if (! filled($value)) {
+                $defaultValue = filled($defaultValue) ? $defaultValue : null;
+                $value = ask("Enter a value for {$key}", $defaultValue);
+            }
 
-        $query = str_replace($replace, $value, $query);
-    }
+            $query = str_replace($replace, $value, $query);
+        });
 
     return trim(str_replace("'", '"', $query));
 }
@@ -74,15 +76,14 @@ function requestHeaders()
 
 function generateSalt()
 {
+    $salt = '';
     $chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#%^&*()-_[]{}<>~+=,.;:/?|';
-    $charOptionLength = strlen($chars) - 1;
 
-    $password = '';
     for ($i = 0; $i < 64; $i++) {
-        $password .= substr($chars, random_int(0, $charOptionLength), 1);
+        $salt .= substr($chars, random_int(0, strlen($chars) - 1), 1);
     }
 
-    return $password;
+    return $salt;
 }
 
 function cleanPath($path)
